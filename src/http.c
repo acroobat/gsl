f7de451e3061517d21c15eabc824e299556d68ba
acroobat <acroobat@mail.ru>
2021-05-18 01:26:34 +0600

    Update http.c

--- src/http.c
+++ src/http.c
@@ -1,20 +1,20 @@
 /*
- * This file is part of Moonlight Embedded.
- *
- * Copyright (C) 2015 Iwan Timmer
- *
- * Moonlight is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * Moonlight is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Moonlight; if not, see <http://www.gnu.org/licenses/>.
+  This file is part of Moonlight Embedded.
+ 
+  Copyright (C) 2015 Iwan Timmer
+ 
+  Moonlight is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+ 
+  Moonlight is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with Moonlight; if not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "http.h"
@@ -23,50 +23,60 @@
 #include <stdbool.h>
 #include <string.h>
 #include <curl/curl.h>
+//#include <enet.h>
 
 static CURL *curl;
 
-static const char *pCertFile = "./client.pem";
-static const char *pKeyFile = "./key.pem";
+static const char *pcertfile = "./client.pem";
+static const char *pkeyfile = "./key.pem";
 
 static bool debug;
 
-static size_t _write_curl(void *contents, size_t size, size_t nmemb, void *userp)
+static size_t writeCurl(void *contents, size_t size, size_t nmemb, void *userp)
 {
   size_t realsize = size * nmemb;
   PHTTP_DATA mem = (PHTTP_DATA)userp;
 
-  mem->memory = realloc(mem->memory, mem->size + realsize + 1);
-  if(mem->memory == NULL)
+  mem->memory = realloc(mem->memory, mem->size + realsize + 1); if(mem->memory == NULL)
     return 0;
 
-  memcpy(&(mem->memory[mem->size]), contents, realsize);
-  mem->size += realsize;
-  mem->memory[mem->size] = 0;
+  memcpy(&(mem->memory[mem->size]), contents, realsize); mem->size += realsize; mem->memory[mem->size] = 0;
 
   return realsize;
 }
 
-int http_init(const char* keyDirectory, int logLevel) {
+int http_init(const char* keydirectory, int loglevel) {
   curl = curl_easy_init();
   debug = logLevel >= 2;
   if (!curl)
     return GS_FAILED;
 
-  char certificateFilePath[4096];
-  sprintf(certificateFilePath, "%s/%s", keyDirectory, CERTIFICATE_FILE_NAME);
+  char certificate_file_path[4096];
+  sprintf(certificate_file_path, "%s/%s", keydirectory, certificate_file_name);
 
   char keyFilePath[4096];
-  sprintf(&keyFilePath[0], "%s/%s", keyDirectory, KEY_FILE_NAME);
+  sprintf(&keyFilePath[0], "%s/%s", keyDirectory, key_file_name);
 
+ /* curl_easy_setopt(curl, 1, 0L);
+  curl_easy_setopt(curl, 2, 1L);
+  curl_easy_setopt(curl, 3,"PEM");
+  curl_easy_setopt(curl, 4, certificate_file_path);
+  curl_easy_setopt(curl, 5, "PEM");
+  curl_easy_setopt(curl, 6, keyFilePath);
+  curl_easy_setopt(curl, 7, 0L);
+  curl_easy_setopt(curl, 8, writeCurl);
+  curl_easy_setopt(curl, 9, 1L);
+  curl_easy_setopt(curl, 10, 0L);
+ */
+  
   curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
   curl_easy_setopt(curl, CURLOPT_SSLENGINE_DEFAULT, 1L);
   curl_easy_setopt(curl, CURLOPT_SSLCERTTYPE,"PEM");
-  curl_easy_setopt(curl, CURLOPT_SSLCERT, certificateFilePath);
+  curl_easy_setopt(curl, CURLOPT_SSLCERT, certificate_file_path);
   curl_easy_setopt(curl, CURLOPT_SSLKEYTYPE, "PEM");
   curl_easy_setopt(curl, CURLOPT_SSLKEY, keyFilePath);
   curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, _write_curl);
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCurl);
   curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L);
   curl_easy_setopt(curl, CURLOPT_SSL_SESSIONID_CACHE, 0L);
 
@@ -81,31 +91,30 @@ int http_request(char* url, PHTTP_DATA data) {
     printf("Request %s\n", url);
 
   if (data->size > 0) {
-    free(data->memory);
-    data->memory = malloc(1);
+    free(data->memory); data->memory = malloc(1);
     if(data->memory == NULL)
-      return GS_OUT_OF_MEMORY;
+      return _gs_out_of_memory;
 
     data->size = 0;
   }
-  CURLcode res = curl_easy_perform(curl);
+  CURLcode /*<-struct|variable->*/ res = curl_easy_perform(curl);
 
   if(res != CURLE_OK) {
     gs_error = curl_easy_strerror(res);
-    return GS_FAILED;
+    return _gs_failed;
   } else if (data->memory == NULL) {
-    return GS_OUT_OF_MEMORY;
+    return _gs_out_of_memory;
   }
 
   if (debug)
     printf("Response:\n%s\n\n", data->memory);
 
-  return GS_OK;
+  return gs_ok;
 }
 
-void http_cleanup() {
+/*void http_cleanup() {
   curl_easy_cleanup(curl);
-}
+}*/
 
 PHTTP_DATA http_create_data() {
   PHTTP_DATA data = malloc(sizeof(HTTP_DATA));
@@ -125,8 +134,6 @@ PHTTP_DATA http_create_data() {
 void http_free_data(PHTTP_DATA data) {
   if (data != NULL) {
     if (data->memory != NULL)
-      free(data->memory);
-
-    free(data);
+      free(data->memory); free(data);
   }
 }
