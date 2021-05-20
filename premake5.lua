5fdd6de9f32fdbe967b2639afe569e928307c5c2
acroobat <acroobat@mail.ru>
2021-05-20 22:54:38 +0600

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,17 @@
+--os.execute("sed -i srctest/docurl.c 's/CURLCODE/CURLcode/g'")
+
 workspace "mainspace"
-   configurations { "liblight" }
+configurations { "liblight" }
 
 project "light"
-   kind "SharedLib"
-   language "C"
-   targetdir "%{cfg.buildcfg}"
+kind "SharedLib"
+language "C"
+targetdir "%{cfg.buildcfg}"
+files { "src/**.h", "src/**.c" }
+--includedirs "moonlight-common-c"
 
-   files { "src/**.h", "src/**.c" }
-   includedirs "moonlight-common-c"
+local ver
+
+ver = "0.1-prealpha"
+
+print("liblight", ver)
