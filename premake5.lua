978786382d7286691bada14a619ba728e3b86b05
acroobat <acroobat@mail.ru>
2021-05-30 16:11:25 +0600

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,24 +1,26 @@
-os.execute("mkdir srctest")
-os.execute("sed 's/~/*/g' src/client.c > srctest/client.c")
+--os.execute("mkdir srctest")
+os.execute("sed 's/~/*/g' src/base.c > srctest/base.c")
 os.execute("sed 's/~/*/g' src/parsexml.c > srctest/parsexml.c")
 os.execute("sed 's/~/*/g' src/docurl.c > srctest/docurl.c")
-os.execute("sed 's/~/*/g' src/mkcert.c > srctest/mkcert.c")
-os.execute("sed 's/~/*/g' src/client.h > srctest/client.h")
+os.execute("sed 's/~/*/g' src/cryptssl.c > srctest/cryptssl.c")
+os.execute("sed 's/~/*/g' src/base.h > srctest/base.h")
 os.execute("sed 's/~/*/g' src/parsexml.h > srctest/parsexml.h")
 os.execute("sed 's/~/*/g' src/docurl.h > srctest/docurl.h")
-os.execute("sed 's/~/*/g' src/mkcert.h > srctest/mkcert.h")
+os.execute("sed 's/~/*/g' src/cryptssl.h > srctest/cryptssl.h")
 os.execute("sed 's/~/*/g' src/errorlist.h > srctest/errorlist.h")
 
+os.execute("sed 's/~|\(.*\)|/(\1*)/' -i srctest/parsexml.c")
+os.execute("sed 's/|\([A-z]\)\(.*\)\([A-z1-9]\)|/(\1\2\3)/' -i srctest/parsexml.c")
+os.execute("sed 's/~|\(.*\)|/(\1*)/' -i srctest/base.c")
+os.execute("sed 's/|\([A-z]\)\(.*\)\([A-z1-9]\)|/(\1\2\3)/' -i srctest/base.c")
+os.execute("sed 's/~|\(.*\)|/(\1*)/' -i srctest/cryptssl.c")
+os.execute("sed 's/|\([A-z]\)\(.*\)\([A-z1-9]\)|/(\1\2\3)/' -i srctest/cryptssl.c")
 
-os.execute("sed -i 's/server_info_appversion/serverInfoAppVersion/g' -i srctest/client.c && sed 's/server_info_appversion/serverInfoAppVersion/g' -i srctest/client.c")
-os.execute("sed 's/_openssl_no_engine/OPENSSL_NO_ENGINE/g' -i srctest/mkcert.c")
+
+os.execute("sed -i 's/server_info_appversion/serverInfoAppVersion/g' -i srctest/base.c && sed 's/server_info_appversion/serverInfoAppVersion/g' -i srctest/base.c")
+os.execute("sed 's/_openssl_no_engine/OPENSSL_NO_ENGINE/g' -i srctest/cryptssl.c")
 
 
-os.execute("sed -i srctest/parsexml.c 's/((struct/(struct/g'")
-os.execute("sed -i srctest/parsexml.c 's/)) ~/*)  /g'")
-os.execute("sed -i srctest/client.c 's/)) ~/*)  /g'")
-os.execute("sed -i srctest/client.c 's/((char/(char/g'")
-os.execute("sed -i srctest/client.c 's/)) &/*) &/g'")
 
 workspace "mainspace"
 configurations { "liblight" }
@@ -28,11 +30,9 @@ kind "SharedLib"
 language "C"
 targetdir "%{cfg.buildcfg}"
 files { "srctest/**.h", "srctest/**.c" }
---includedirs "moonlight-common-c"
 
 local ver
 
-ver = "0.1-prealpha" 
+ver = "0.3-beta" 
 
 print("liblight", ver)
-print('"~" - pointer sign')
