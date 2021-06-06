0877de8488cd078ac8b19c7c925b6c30de08949e
acroobat <acroobat@mail.ru>
2021-06-06 23:55:31 +0600

    Update lightplug.c

--- plug/src/lightplug.c
+++ plug/src/lightplug.c
@@ -6,8 +6,8 @@
 
 #include <mpv/client.h>
 
-#include <libgslight/base.h>
-#include <libgslight/base.h>
+#include <gsl/base.h>
+#include <Limelight.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -18,9 +18,21 @@ bool startsWith(const char *a, const char *b) {
     return 0;
 }
 
+static int mpv_renderer_setup(int videoFormat, int width, int height, int redrawRate, void *context, int drFlags) {};
+
+void mpv_cleanup() {
+}
+
+int mpv_submit_decode_unit(PDECODE_UNIT decodeUnit) {}
+
 int mpv_open_cplugin(mpv_handle *handle) {
-    //GSlight_Init()
-    //GSlight_Applist()
+    PGSL_DATA server;
+    //GSl_Init()
+    //GSl_Pair()
+    //GSl_Applist()
+    //GSl_StartApp()
+    
+    //LiStartConnection(&server->serverinfo, &config->stream, &connection_callbacks, (DECODER_RENDERER_CALLBACKS)decoder_callbacks_mpv, platform_get_audio(system, config->audio_device), NULL, drFlags, config->audio_device, 0);
     while (1) {
         mpv_event *event = mpv_wait_event(handle, -1);
         char *result = NULL;
@@ -43,7 +55,21 @@ int mpv_open_cplugin(mpv_handle *handle) {
     return 0;
 }
 
+
+DECODER_RENDERER_CALLBACKS decoder_callbacks_mpv = {
+  .setup = mpv_renderer_setup,
+  .cleanup = mpv_renderer_cleanup,
+  .submitDecodeUnit = mpv_submit_decode_unit,
+  .capabilities = CAPABILITY_DIRECT_SUBMIT,
+};
+    
+AUDIO_RENDERER_CALLBACKS audio_callbacks_alsa = {
+  .init = alsa_renderer_init,
+  .cleanup = alsa_renderer_cleanup,
+  .decodeAndPlaySample = alsa_renderer_decode_and_play_sample,
+  .capabilities = CAPABILITY_DIRECT_SUBMIT,
+};
+
 #ifdef __cplusplus
 }
 #endif
-
